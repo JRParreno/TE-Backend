7fd0c9edcafdf8ea80a384147436dea918d44bb7
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-15 20:26:10 +0800

    Add users models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# for multi user
+# inherit models USER
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.user.firstname
+
+
+# for student models
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    student_number = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return self.student_number
+
+
+# for professor
+class Professor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    faculty_id = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return self.faculty_id
