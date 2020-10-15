ba49e4be26d1f6569172a493d4d587338c1c182c
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-15 20:38:53 +0800

    Initial setup of models

--- chapters/models.py
+++ chapters/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Section.models import Section
 
-# Create your models here.
+class Chapter(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    embbeded_url = models.CharField(max_length=255, null=True, blank=True)
+    note = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
