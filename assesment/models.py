6b76706d3a12607b59bdc7ea5a04389233fddb53
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-16 00:34:47 +0800

    Add assesment models

--- assesment/models.py
+++ assesment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from activity.models import Activity
 
-# Create your models here.
+
+class Assesment(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, null=True, blank=False)
+    student = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
+    score = models.FloatField(null=True, blank=False)
+
+    def __str__(self):
+        return self.score
--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'sections',
     'activity',
     'question',
+    'assesment',
 ]
 
 MIDDLEWARE = [
