e20d7e5923a1260f4ded3c846565e3cbbf533b23
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-15 17:15:59 +0800

    Django Setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'electives_db',
+        'USER': 'postgres',
+        'PASSWORD': '121015',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
