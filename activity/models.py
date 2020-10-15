d9745e02398064a372aaabcc9fb2d62d30fc4cf2
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-15 23:16:24 +0800

    Add activity models

--- activity/models.py
+++ activity/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from sections.models import Section
+from datetime import datetime
 
-# Create your models here.
+# activities
+class Activity(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=False)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True, blank=False)
+    start = models.DateTimeField(default=datetime.now(), null=True, blank=False)
+    end = models.DateTimeField(default=datetime.now(), null=True, blank=False)
+    remarks = models.BooleanField(null=True, blank=True)
