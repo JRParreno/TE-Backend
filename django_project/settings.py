1531b8dedf41d64c48b7070be1f94f012cc65fab
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-19 19:04:56 +0800

    change email config

--- django_project/settings.py
+++ django_project/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'jhonrhayparreno22@gmail.com'
+EMAIL_HOST_PASSWORD = 'PythonDjango22'
 
 django_heroku.settings(locals())
