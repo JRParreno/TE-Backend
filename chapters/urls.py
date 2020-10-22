3b8f90c217d6407a28fcef879df240e34603bdac
jhonrhay <35423806+JRParreno@users.noreply.github.com>
2020-10-22 21:34:22 +0800

    Add chpater api and feedback

--- chapters/urls.py
+++ chapters/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
-from .views import ChapterAPIView
+from .views import ChapterViewSet, ChapterFeedbackAPIView, StudentRemarksAPIView
 from rest_framework import routers
 router = routers.DefaultRouter()
-router.register('list', ChapterAPIView, basename='chapter')
+router.register('list', ChapterViewSet, basename='chapter')
 
 urlpatterns = [
     path('viewset/', include(router.urls)),
     path('viewset/<int:pk>/', include(router.urls)),
+    path('feedback/', ChapterFeedbackAPIView.as_view()),
+    path('feedback/<chapter>/', ChapterFeedbackAPIView.as_view()),
+    path('complete/', StudentRemarksAPIView.as_view()),
+    path('complete/<section>/', StudentRemarksAPIView.as_view()),
 ]
