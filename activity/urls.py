a44bd8cc6490196ddb5856f05e70e7ecfedc7ca7
Jhon Rhay Parreno <jhonrhayparreno22@gmail.com>
2020-11-20 23:10:31 +0800

    Add student per activity view

--- activity/urls.py
+++ activity/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ActivityViewSet, ProfActivityViewSet, ActivityTypeAPIView
+from .views import ActivityViewSet, StudentActivityAPIView,ProfActivityViewSet, ActivityTypeAPIView
 from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('list', ActivityViewSet, basename='activity')
@@ -9,4 +9,5 @@ urlpatterns = [
     path('viewset/<int:activity_type>/', include(router.urls)),
     path('viewset/<int:activity_type>/<int:pk>/', include(router.urls)),
     path('type/', ActivityTypeAPIView.as_view()),
+    path('student/<int:activity_type>/', StudentActivityAPIView.as_view()),
 ]
--- assesment/views.py
+++ assesment/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import AssesmentSerializer
+from .models import Assesment
+from rest_framework import status, views, permissions, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+# activity type
+class AssesmentAPIView(generics.ListAPIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Assesment.objects.all()
+    serializer_class = AssesmentSerializer
