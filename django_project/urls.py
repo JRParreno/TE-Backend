1024831ea98ca032a34ae3561b0c8b1977b24571
Jhon Rhay Parreno <jhonrhayparreno22@gmail.com>
2020-12-01 08:34:35 +0800

    Fix download files

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt import views as jwt_views
 
 
@@ -35,3 +37,7 @@ urlpatterns = [
     path('api/student/', include('student.urls')),
     path('api/user/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
